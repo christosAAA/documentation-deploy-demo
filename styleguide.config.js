96476324d70f85890592c1c2ebb16f079e1bfe40
Christos Atzinas <christos.atzinas@gmail.com>
2022-03-18 19:25:35 +0200

    llalal

---
+++ styleguide.config.js
@@ -0,0 +1,74 @@
+module.exports = {
+  title: 'TCC Frontend App',
+  defaultExample: true,
+  pagePerSection: true,
+  sections: [
+    {
+      name: 'Frontend WebApp',
+      // content: 'ImagesAndIcons.md',
+      sections: [
+        {
+          name: 'About repo',
+          // content: 'src/components/icons/Readme.md',
+        },
+        {
+          name: 'Project Layout',
+          // content: 'src/media/Readme.md',
+        },
+        {
+          name: 'Usage',
+          // content: 'README.md',
+        },
+      ],
+    },
+    {
+      name: 'Best Practices',
+      // content: 'ImagesAndIcons.md',
+      sections: [
+        {
+          name: 'Folder Structure and Naming',
+          // content: 'README.md',
+        },
+        {
+          name: 'Material UI usage',
+          // content: 'README.md',
+        },
+        {
+          name: 'Translation',
+          // content: 'README.md',
+        },
+        {
+          name: 'Routing',
+          // content: 'README.md',
+        },
+        {
+          name: 'State managing',
+          // content: 'README.md',
+        },
+        {
+          name: 'Url Query Params ',
+          // content: 'README.md',
+        },
+        {
+          name: 'Testing',
+          // content: 'src/media/Readme.md',
+        },
+        {
+          name: 'SVGs',
+          // content: 'src/components/icons/Readme.md',
+        },
+        {
+          name: 'PNGs',
+          // content: 'src/media/Readme.md',
+        },
+      ],
+    },
+
+    {
+      name: 'Getting Started with Create React App',
+      // content: 'README.md',
+    },
+
+
+  ],
+};
